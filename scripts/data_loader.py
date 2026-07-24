c5be15aba911c6d0ba6ebbf7d8c401c7b4112dc7
Bhushan Deshpande <bhushan1729dsp@gmail.com>
2026-07-24 23:54:51 +0530

    fix: load glue dataset via official namespaced repository nyu-mll/glue to avoid hf-hub URI errors

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -190,7 +190,7 @@ def get_data_loaders(dataset_name='MNIST', batch_size=64, data_dir='./data', tra
             tokenizer = AutoTokenizer.from_pretrained(transformer_model, use_fast=False)
             
             if dataset_name == 'SST2':
-                hf_dataset = load_dataset("glue", "sst2", cache_dir=data_dir)
+                hf_dataset = load_dataset("nyu-mll/glue", "sst2", cache_dir=data_dir)
                 text_key = 'sentence'
             else:
                 hf_dataset = load_dataset("imdb", cache_dir=data_dir)
